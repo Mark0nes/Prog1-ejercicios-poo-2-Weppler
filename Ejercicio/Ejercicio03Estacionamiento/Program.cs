fe96a53397ed19b862a6138aa59d0a5101a635e5
Marcos <wepplermarcosa@gmail.com>
2026-04-20 23:14:30 -0300

    Agregado ejercicio 3

---
+++ Ejercicio/Ejercicio03Estacionamiento/Program.cs
@@ -0,0 +1,7 @@
+/*Un estacionamiento privado necesita controlar los vehículos que ingresan y egresan.
+Cada vehículo tiene una patente y el nombre de su dueño.
+El estacionamiento tiene una cantidad máxima de lugares.
+El sistema debe registrar el ingreso de un vehículo (solo si hay lugar disponible),
+registrar su egreso, verificar si un vehículo con una determinada patente está estacionado en este momento,
+e informar cuántos lugares libres quedan.*/
+
